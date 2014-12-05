3db273f6437deabbcc4dee2405532eb1ca227d38
jai11 <jai.jagani@yahoo.in>
2014-12-06 04:30:02 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ git init
 git add *
 git commit -m "first commit"
 git remote remove origin
-git remote add origin https://jai11:aug1234@github.com/jai11/jenkins_job.git
+git remote add origin https://jai11:@github.com/jai11/jenkins_job.git
 git push -u origin master
 
